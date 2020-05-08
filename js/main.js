03bd3509d087a0a720828f853941c5ef4e6df827
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-05-09 00:59:07 +0200

    first update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+console.log("boom");
+
+
+// menü function sidebar
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+$(document).ready(function(){
+	$("body").fadeIn(900);
+
+})
